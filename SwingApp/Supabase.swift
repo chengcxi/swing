0c0196b93f813a76e82e7644672f9fbe50450fad
Cheng <cxi@ucsb.edu>
2026-02-17 17:08:32 -0800

    supabase integration

---
+++ SwingApp/Supabase.swift
@@ -0,0 +1,8 @@
+import Foundation
+import Supabase
+
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://qiunnjybpsvfohbfvxco.supabase.co")!,
+  supabaseKey: "sb_publishable_w6lLCBpfWTqKmKgwLXJloQ__3rqOGyf"
+)
+        
